a29cbcaa357a1a5206f78a6576536afb163fb8da
visharma123 <binodsharma1014@gmail.com>
2022-12-22 18:33:36 +0530

    inital commit

---
+++ emplyee-management-system-ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import AddEmployee from './components/AddEmployee';
+
+function App() {
+
+  return (
+    <>
+  <Navbar></Navbar>
+  <AddEmployee></AddEmployee>
+  </>
+  )
+}
+
+export default App;
