8fa35c0cb1c52d5040ab1d51340be7d894fee9f8
visharma123 <binodsharma1014@gmail.com>
2022-12-23 22:44:32 +0530

    final code added

--- emplyee-management-system-ui/src/App.js
+++ emplyee-management-system-ui/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import AddEmployee from './components/AddEmployee';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
+import UpdateEmployee from './components/UpdateEmployee';
 
 function App() {
 
   return (
     <>
-  <Navbar></Navbar>
-  <AddEmployee></AddEmployee>
+    <BrowserRouter>
+    <Navbar></Navbar>
+    <Routes>
+      <Route index element={<EmployeeList />} />
+      <Route path="/" element={<EmployeeList />}></Route>
+      <Route path="/employeeList" element={<EmployeeList></EmployeeList>} />
+      <Route path = "/addEmployee" element = {<AddEmployee />} />
+      <Route path = "/editEmployee/:id" element={<UpdateEmployee />} />
+    </Routes>
+    </BrowserRouter>
+  
   </>
   )
 }
--- emplyee-management-system-ui/src/components/AddEmployee.js
+++ emplyee-management-system-ui/src/components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
@@ -10,6 +11,8 @@ const AddEmployee = () => {
         emailId:""
     });
 
+    const navigate = useNavigate();
+
     const handleChange = (e) =>{
         const value = e.target.value;
         setEmployee({...employee, [e.target.name]: value});
@@ -19,11 +22,22 @@ const AddEmployee = () => {
         e.preventDefault();
         EmployeeService.saveEmployee(employee).then((respose)=>{
             console.log(respose);
+            navigate("/employeelist")
         })
         .catch((error) => {
             console.log(error);
         });
     };
+
+    const reset = (e) => {
+        e.preventDefault();
+        setEmployee({
+        id: "",
+        firstName: "",
+        lastName:"",
+        emailId:""
+        });
+    }
 
   return (
     <div className='flex max-w-2xl mx-auto shadow border-b'>
@@ -66,7 +80,7 @@ const AddEmployee = () => {
             </div>
             <div className='item-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
                 <button onClick={saveEmployee} className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>Save</button>
-                <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6'>Clear</button>
+                <button onClick={reset} className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6'>Clear</button>
             </div>
         </div>
     </div>
